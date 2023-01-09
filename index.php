f331bf0360e25be2b3de178fd6bd1617dc5bbca4
RominaMalimban <romina.lyn@hotmail.it>
2023-01-09 15:24:00 +0100

    creazione variabile p e stampo p e lunghezza

---
+++ index.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+
+<body>
+    <p>
+        <?php
+        $myVariable = "Lorem ipsum dolor sit amet consectetur adipisicing elit. Similique accusantium quia, doloribus   
+            aperiam laborum praesentium voluptas esse in iste voluptatibus obcaecati quae vitae incidunt porro nulla 
+            reprehenderit sit officia veritatis.";
+        echo $myVariable;
+        echo strlen($myVariable);
+        ?>
+
+    </p>
+
+</body>
+
+</html>
